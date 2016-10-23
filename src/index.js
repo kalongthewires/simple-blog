e21c7837a204100b439062aa2b5b280804909d53
Kim Hamper <khamper@base2.io>
2016-10-23 15:01:28 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './components/app';
+import configureStore from './configureStore';
+
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={ store }>
+    <App />
+  </Provider>,
+  document.querySelector('.container'));
